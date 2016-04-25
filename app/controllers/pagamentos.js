b49da8461de50730f47e75a38ca6743f3f751b85
vinifuzetti <cust6193@caelum131-12>
2016-04-25 17:00:31 -0300

    Final do primeiro dia

--- app/controllers/pagamentos.js
+++ app/controllers/pagamentos.js
@@ -7,8 +7,48 @@ module.exports = function(app){
 
   //os parametros da funcao obedece a ordem dos paramentros
   app.post('/pagamentos/pagamento', function(req, res){
-    var body = req.body;
-    res.json(body);
-    console.log('Ok');
+    //validacao dos dados recebidos
+    req.assert('forma_de_pagamento','Indique a forma de pagamento').notEmpty();
+    req.assert('valor', 'Indique o valor numerico').notEmpty().isFloat();
+    req.assert('moeda', 'Indique a moeda no formato BRL').notEmpty().len(3,3);
+    var errors = req.validationErrors();
+    if (errors){
+      console.log("Erros de validação encontrados");
+      res.status(400).json(errors);
+      return;
+    }
+
+    var pagamento = req.body;
+    var connection = app.infra.connectionFactory();
+    var pagamentoDAO = new app.infra.PagamentoDao(connection);
+
+    pagamento.status = 'CRIADO';
+    pagamento.data = new Date;
+    pagamentoDAO.salva(pagamento, function(error, result){
+      if(error){
+        res.status(500).send(error);
+        return;
+      }
+      pagamento.id = result.insertId;
+      //console.log('pagamento criado com sucesso:	'	+	JSON.stringify(pagamento));
+      console.log('Pagamento criado com ID: ' + pagamento.id);
+      res.location('/pagamentos/pagamento/' + pagamento.id)
+      var	response	=	{
+          dados_do_pagamento:	pagamento,
+          links:	[{
+                    href:	"http://localhost:3000/pagamentos/pagamento/"	+	pagamento.id,
+                    rel:	"confirmar",
+                    method:	"PUT"
+                    },
+                    {
+                    href:	"http://localhost:3000/pagamentos/pagamento/"	+	pagamento.id,
+                    rel:	"cancelar",
+                    method:	"DELETE"
+          }]
+      }
+      res.status(201).json(response);
+    });
+    //res.json(pagamento);
+    //console.log('Ok');
   });
 }
--- config/express.js
+++ config/express.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var load = require('express-load');
 var bp = require('body-parser');
+var validator = require('express-validator');
 
 module.exports = function(){
   var app = express();
   //os use devem ser feitos antes de carregar as rotas
   app.use(bp.json());
   app.use(bp.urlencoded({extended: true}));
+  app.use(validator());
+
   //carrega as rotas sem precisar dar require do express nas rotas
-  load('controllers', {cwd: 'app'}).into(app);
+  load('controllers', {cwd: 'app'})
+        .then('infra')
+        .into(app);
 
   return app;
 }
